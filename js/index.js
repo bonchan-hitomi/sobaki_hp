91185024521382240158117bb444169af480604c
Hitomi <0hikarimement@gmail.com>
2020-08-29 20:34:28 +0900

    commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function(){
   var setImg = '.fade-picture';
   var fadeSpeed = 3000;
   var switchDelay = 2000;
@@ -32,4 +32,17 @@ $(function(){
       }
     });
   });
+});
+
+$(function(){
+    $(window).scroll(function (){
+        $('.okan2').each(function(){
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 200){
+                $(this).addClass('scrollin');
+            }
+        });
+    });
 });
